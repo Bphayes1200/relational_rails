84e984c50706090fc30af7834e6add65bb285a4e
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-07 10:37:50 -0700

    Test: add test for number of car count per dealership

--- spec/models/dealership_spec.rb
+++ spec/models/dealership_spec.rb
@@ -2,16 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Dealership, type: :model do 
   it {should have_many :cars}
-end 
 
-describe "class methods" do 
-  describe ':: order_by_created_at' do 
-    it 'will order the dealerships by the most recently created' do 
+  describe "class methods" do 
+    describe ':: order_by_created_at' do 
+      it 'will order the dealerships by the most recently created' do 
+        dealership_1 = Dealership.create(name: "BH Ford", city: "Boulder", rank: 1, service_shop: true)
+        dealership_2 = Dealership.create(name: "Mountain Lexus", city: "Broomfield", rank: 2, service_shop: false)
+        dealership_3 = Dealership.create(name: "CH Toyota", city: "Denver", rank: 3, service_shop: true)
+
+        expect(Dealership.order_by_created_at).to eq([dealership_3, dealership_2, dealership_1])
+      end
+    end
+  end
+
+  describe "instance methods" do 
+    describe '#num_of_cars' do 
       dealership_1 = Dealership.create(name: "BH Ford", city: "Boulder", rank: 1, service_shop: true)
-      dealership_2 = Dealership.create(name: "Mountain Lexus", city: "Broomfield", rank: 2, service_shop: false)
-      dealership_3 = Dealership.create(name: "CH Toyota", city: "Denver", rank: 3, service_shop: true)
+      car_1 = Car.create!(make: "Ford", model: "F150", year: 2014, accident: true, mileage: 80000, dealership_id: dealership_1[:id])
+      car_2 = Car.create!(make: "Ford", model: "Fusion", year: 2014, accident: true, mileage: 80000, dealership_id: dealership_1[:id])
 
-      expect(Dealership.order_by_created_at).to eq([dealership_3, dealership_2, dealership_1])
+      expect(dealership_1.num_of_cars).to eq(2)
     end
   end
 end
+
