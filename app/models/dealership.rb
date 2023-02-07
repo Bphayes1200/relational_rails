94603be0bb36051e527fc41886807c346ac37772
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-07 10:02:37 -0700

    Feat: add order_by_created_at method

--- app/models/dealership.rb
+++ app/models/dealership.rb
@@ -1,3 +1,7 @@
 class Dealership < ApplicationRecord
   has_many :cars
+
+  def self.order_by_created_at
+    order(created_at: :DESC).to_a
+  end
 end
--- spec/models/dealership_spec.rb
+++ spec/models/dealership_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Dealership, type: :model do 
   it {should have_many :cars}
+end 
 
+describe "class methods" do 
   describe ':: order_by_created_at' do 
     it 'will order the dealerships by the most recently created' do 
       dealership_1 = Dealership.create(name: "BH Ford", city: "Boulder", rank: 1, service_shop: true)
       dealership_2 = Dealership.create(name: "Mountain Lexus", city: "Broomfield", rank: 2, service_shop: false)
       dealership_3 = Dealership.create(name: "CH Toyota", city: "Denver", rank: 3, service_shop: true)
-    
-      expect(Dealership.order_by_created_at).to eq([dealership_1, dealership_2, dealership_3])
+
+      expect(Dealership.order_by_created_at).to eq([dealership_3, dealership_2, dealership_1])
     end
   end
 end
